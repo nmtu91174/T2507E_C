7ce3040ec1388e0e62193de54d3664f5a3af5e9e
nmtu91174 <nmtu91174@gmail.com>
2025-08-13 00:58:15 +0700

    so nguyen to trong khoang a b

---
+++ homeWorksS6/so_NT_a_to_b.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b;
+	
+	
+	do {
+		
+		printf("\nHay nhap vao hai so a va b (a<b):\n");
+		
+		printf("\nNhap so a: ");
+		scanf("%d", &a);
+		
+		printf("\nNhap so b: ");
+		scanf("%d", &b);
+		
+			if(a>=b){
+				printf("\n>>>> a phai <= b! <<<\n\n==> Hay nhap lai! <==\n");
+			} 
+			
+	} while(a>=b);
+	
+	for(int i = a; i <= b; i++){
+		
+		int c = 0;
+				
+		for(int u = 1; u <= i; u++){
+			
+			if(i % u == 0){
+				c++;
+			}
+		}
+						
+		if(c==2){
+			printf("\nSo nguyen to (i) la: %d", i);
+		}
+			
+	}
+	
+	return 0;		
+}
+	
+	
+
+
+		
+
+	
+	
+	
+	
