86434bececa84127a962a9a7d93092aad5d15336
nmtu91174 <nmtu91174@gmail.com>
2025-08-18 16:03:23 +0700

    cach 2 tim  -x,x

--- homeWorksS7_plus/doan_xx_chua_mang.cpp
+++ homeWorksS7_plus/doan_xx_chua_mang.cpp
@@ -28,32 +28,41 @@ int main(){
 	
 	printf("\n------------------------------------\n");
 	
-	int min = a[0];
+	//int min = a[0];
 	int max = a[0];
 	
-	for(int i=0; i<n; i++){
-		if(a[i] < min){
-			min = a[i];
-		}
-		
-		if(a[i] > max){
-			max = a[i];
+//	for(int i=0; i<n; i++){
+//		if(a[i] < min){
+//			min = a[i];
+//		}
+//		
+//		if(a[i] > max){
+//			max = a[i];
+//		}
+//	}
+//	
+//	for(int i=0; i <n; i++){
+//	
+//		if(abs(min) > abs(max)){
+//			x = abs(min);
+//		} else if (abs(min) < abs(max)){
+//			x = abs(max);
+//			} else {
+//				x = abs(min);
+//			}
+//	}
+	
+	for(int i=0; i<n; i++ ){
+		if(abs(a[i]) > max){
+			
+			max = abs(a[i]);
 		}
 	}
 	
-	for(int i=0; i <n; i++){
-	
-		if(abs(min) > abs(max)){
-			x = abs(min);
-		} else if (abs(min) < abs(max)){
-			x = abs(max);
-			} else {
-				x = abs(min);
-			}
-	}
+	x = max;
 	
 	printf("\nGia tri cua x la: %d", x);
-	printf("\nDoan [-x,x]: [-%d,%d]", x,x);
+	printf("\nDoan [-x,x]: [-%d,%d] chua ta ca cac gia tri trong a[n]!", x,x);
 	printf("\n");
 	
 	return 0;
